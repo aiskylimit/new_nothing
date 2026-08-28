40a135254fb7e1a347900f24efb2d421278a2232
phongdq25 <nothing>
2026-08-29 00:29:39 +0700

    update

--- commands.sh
+++ commands.sh
@@ -6,8 +6,7 @@
 # nvidia-smi
 # CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 python3 /tmp/llm_pretrain_burn.py &
 
-# kill -9 $(nvidia-smi --query-compute-apps=pid --format=csv,noheader)
-pkill -f llm_pretrain_burn 2>/dev/null || true
+kill -9 $(nvidia-smi -i 4,5,6,7 --query-compute-apps=pid --format=csv,noheader)
 # sleep 3
 nvidia-smi
 
--- commands_backup.sh
+++ commands_backup.sh
@@ -1,23 +1,24 @@
 #2
-#segd
-#v2
+#reward
+#v1
 
 
 # nvidia-smi
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 python3 /tmp/llm_pretrain_burn.py &
 
-kill -9 $(nvidia-smi --query-compute-apps=pid --format=csv,noheader)
-sleep 5
+kill -9 $(nvidia-smi -i 4,5,6,7 --query-compute-apps=pid --format=csv,noheader)
+# sleep 3
 nvidia-smi
 
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
-
-
 # cd ./talas_vlm_embed
 # bash ./project_commands.sh
 
 
-cd ./spectral-guided-learning
+# cd ./spectral-guided-learning
+# bash ./project_commands.sh
+
+cd ./reward-guidance-main
 bash ./project_commands.sh
