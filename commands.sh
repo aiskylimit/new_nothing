abaed20c87ddf186b096f3dd689ba126ecc24490
phongdq25 <nothing>
2026-08-29 00:33:04 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,24 +1,14 @@
-#1 +10
-#reward
-#v1
-
-
-# nvidia-smi
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 python3 /tmp/llm_pretrain_burn.py &
-
-# kill -9 $(nvidia-smi -i 4,5,6,7 --query-compute-apps=pid --format=csv,noheader)
-# sleep 3
-nvidia-smi
-
-export PATH=/usr/local/cuda/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
-
-# cd ./talas_vlm_embed
-# bash ./project_commands.sh
-
-
-# cd ./spectral-guided-learning
-# bash ./project_commands.sh
-
-# cd ./reward-guidance-main
-# bash ./project_commands.sh
+#d
+#datasets
+--url https://huggingface.co/datasets/TIGER-Lab/MMEB-train/resolve/main/images_zip/ImageNet_1K.zip /mnt/local/aiskylimit_new_nothing/talas_vlm_embed/datasets
+--url https://huggingface.co/datasets/TIGER-Lab/MMEB-train/resolve/main/images_zip/HatefulMemes.zip /mnt/local/aiskylimit_new_nothing/talas_vlm_embed/datasets
+--url https://huggingface.co/datasets/TIGER-Lab/MMEB-train/resolve/main/images_zip/VOC2007.zip /mnt/local/aiskylimit_new_nothing/talas_vlm_embed/datasets
+--url https://huggingface.co/datasets/TIGER-Lab/MMEB-train/resolve/main/images_zip/N24News.zip /mnt/local/aiskylimit_new_nothing/talas_vlm_embed/datasets
+--url https://huggingface.co/datasets/TIGER-Lab/MMEB-train/resolve/main/images_zip/SUN397.zip /mnt/local/aiskylimit_new_nothing/talas_vlm_embed/datasets
+--url https://huggingface.co/datasets/VoCuc/vlm-teacher-embedding/resolve/main/B3_Qwen2_2B_cls.tar.gz /mnt/local/aiskylimit_new_nothing/talas_vlm_embed/datasets
+--url https://huggingface.co/datasets/TIGER-Lab/MMEB-eval/resolve/main/images.zip /mnt/local/aiskylimit_new_nothing/talas_vlm_embed/datasets
+#models
+--hf raghavlite/B3_Qwen2_2B /mnt/local/aiskylimit_new_nothing/talas_vlm_embed/models/B3_Qwen2_2B
+--hf raghavlite/B3_Qwen2_7B /mnt/local/aiskylimit_new_nothing/talas_vlm_embed/models/B3_Qwen2_7B
+--hf apple/FastVLM-0.5B mnt/local/aiskylimit_new_nothing/talas_vlm_embed/models/FastVLM-0.5B
+--hf llava-hf/llava-onevision-qwen2-0.5b-ov-hf mnt/local/aiskylimit_new_nothing/talas_vlm_embed/models/llava-onevision-qwen2-0.5b-ov-hf
