03bb0477cedb82251c57484fdde406b03e3c087f
phongdq25 <nothing>
2026-08-28 23:05:21 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
 #1 +30
-#burn
+#reward
 #v1
 
 
@@ -7,14 +7,12 @@
 # CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 python3 /tmp/llm_pretrain_burn.py &
 
 # kill -9 $(nvidia-smi --query-compute-apps=pid --format=csv,noheader)
-# pkill -f llm_pretrain_burn 2>/dev/null || true
+pkill -f llm_pretrain_burn 2>/dev/null || true
 # sleep 3
 nvidia-smi
 
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
-
-pwd
 
 # cd ./talas_vlm_embed
 # bash ./project_commands.sh
@@ -22,3 +20,6 @@ pwd
 
 # cd ./spectral-guided-learning
 # bash ./project_commands.sh
+
+cd ./reward-guidance-main
+bash ./project_commands.sh
