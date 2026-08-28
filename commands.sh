f7419063a21d9bdccb7bbd88e9f640a9874eb070
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-08-28 16:37:11 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,9 +1,4 @@
 #d
-#models
---hf black-forest-labs/FLUX.1-dev /mnt/local/_models/@PROJECT@/FLUX.1-dev
---hf gabeguofanclub/flux-1-dev-flowmap-lsd /mnt/local/_models/@PROJECT@/flux-1-dev-flowmap-lsd
---hf THUDM/ImageReward /mnt/local/_models/@PROJECT@/ImageReward
---hf google-bert/bert-base-uncased /mnt/local/_models/@PROJECT@/bert-base-uncased
---hf Qwen/Qwen2.5-VL-3B-Instruct /mnt/local/_models/@PROJECT@/Qwen2.5-VL-3B-Instruct
+#datasets
 --url https://openaipublic.azureedge.net/clip/models/40d365715913c9da98579312b702a82c18be219cc2a73407c4526f58eba950af/ViT-B-32.pt /mnt/local/_models/@PROJECT@/clip/ViT-B-32.pt
 
