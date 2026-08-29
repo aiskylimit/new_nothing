39ed2b90d2e1ae05d4763c179feb52a51d38d9be
phongdq25 <nothing>
2026-08-29 10:07:23 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,4 +1,3 @@
 #d
 #models
---hf apple/FastVLM-0.5B /mnt/local/aiskylimit_new_nothing/talas_vlm_embed/models/FastVLM-0.5B
 --hf llava-hf/llava-onevision-qwen2-0.5b-ov-hf /mnt/local/aiskylimit_new_nothing/talas_vlm_embed/models/llava-onevision-qwen2-0.5b-ov-hf
