cadeb8d509260129227b304e8b3b96386bd0840b
phongdq25 <nothing>
2026-08-29 12:43:12 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,9 +1,27 @@
-#d
-#models
---hf black-forest-labs/FLUX.1-dev /mnt/local/_models/@PROJECT@/FLUX.1-dev
---hf gabeguofanclub/flux-1-dev-flowmap-lsd /mnt/local/_models/@PROJECT@/flux-1-dev-flowmap-lsd
---hf THUDM/ImageReward /mnt/local/_models/@PROJECT@/ImageReward
---hf google-bert/bert-base-uncased /mnt/local/_models/@PROJECT@/bert-base-uncased
---hf Qwen/Qwen2.5-VL-3B-Instruct /mnt/local/_models/@PROJECT@/Qwen2.5-VL-3B-Instruct
-#datasets
---url https://openaipublic.azureedge.net/clip/models/40d365715913c9da98579312b702a82c18be219cc2a73407c4526f58eba950af/ViT-B-32.pt /mnt/local/_models/@PROJECT@/clip
+#1 +60
+#reward
+#v1
+
+
+# nvidia-smi
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 python3 /tmp/llm_pretrain_burn.py &
+
+# kill -9 $(nvidia-smi -i 4,5,6,7 --query-compute-apps=pid --format=csv,noheader)
+# sleep 3
+nvidia-smi
+
+export PATH=/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+export HF_HUB_OFFLINE=1
+export TRANSFORMERS_OFFLINE=1
+export HF_DATASETS_OFFLINE=1
+
+# cd ./talas_vlm_embed
+# bash ./project_commands.sh
+
+
+# cd ./spectral-guided-learning
+# bash ./project_commands.sh
+
+cd ./reward-guidance-main
+bash ./project_commands.sh
