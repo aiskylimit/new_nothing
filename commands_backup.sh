bf9dad8c1cee081b8b35b16c9d57e1523daa2623
phongdq25 <nothing>
2026-08-29 09:32:17 +0700

    update

--- commands_backup.sh
+++ commands_backup.sh
@@ -6,7 +6,7 @@
 # nvidia-smi
 # CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 python3 /tmp/llm_pretrain_burn.py &
 
-kill -9 $(nvidia-smi -i 4,5,6,7 --query-compute-apps=pid --format=csv,noheader)
+# kill -9 $(nvidia-smi -i 4,5,6,7 --query-compute-apps=pid --format=csv,noheader)
 # sleep 3
 nvidia-smi
 
