278cee2259de76d850c55a52c2246aa167080498
DiningSystem <81498410+DiningSystem@users.noreply.github.com>
2026-08-29 11:55:40 +0700

    edit_path

--- VLM_Distillation-main/project_commands.sh
+++ VLM_Distillation-main/project_commands.sh
@@ -7,6 +7,12 @@ uv sync --locked
 
 source vlm_distill/bin/activate
 
+unzip -q -o train_data/coco/train2017.zip -d coco
+unzip -q -o train_data/gqa/images.zip -d gqa
+unzip -q -o train_data/textvqa/train_val_images.zip -d textvqa
+unzip -q -o train_data/vg/images.zip -d vg
+unzip -q -o train_data/vg/images2.zip -d vg
+
 #bash download_datatrain.sh
 
 export CUDA_VISIBLE_DEVICES=4,5
