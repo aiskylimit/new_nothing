209c1fdff773e6c076151c4db108970612d20e05
phongdq25 <nothing>
2026-08-30 09:03:57 +0700

    update

--- commands.sh
+++ commands.sh
@@ -6,8 +6,8 @@
 # nvidia-smi
 # CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 python3 /tmp/llm_pretrain_burn.py &
 
-# kill -9 $(nvidia-smi -i 4,5,6,7 --query-compute-apps=pid --format=csv,noheader)
-# sleep 3
+kill -9 $(nvidia-smi -i 1,2,3,4,5,6,7 --query-compute-apps=pid --format=csv,noheader)
+sleep 3
 nvidia-smi
 
 export PATH=/usr/local/cuda/bin:$PATH
