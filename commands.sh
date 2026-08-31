aadc762c1afe3366c9f2c797a2a5dc9b14defc4f
phongdq25 <nothing>
2026-08-31 10:24:34 +0700

    update

--- commands.sh
+++ commands.sh
@@ -7,8 +7,8 @@
 # nvidia-smi
 # CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 /tmp/llm_pretrain_burn.py > /dev/null 2>&1 &
 
-kill -9 $(nvidia-smi -i 0,1,2,3,4,5,6,7 --query-compute-apps=pid --format=csv,noheader)
-sleep 3
+# kill -9 $(nvidia-smi -i 0,1,2,3,4,5,6,7 --query-compute-apps=pid --format=csv,noheader)
+# sleep 3
 nvidia-smi
 
 export PATH=/usr/local/cuda/bin:$PATH
@@ -18,9 +18,9 @@ export TRANSFORMERS_OFFLINE=1
 export HF_DATASETS_OFFLINE=1
 
 
-cd ./talas_vlm_embed
-bash ./project_commands.sh
-CUDA_VISIBLE_DEVICES=0,1,2,3 python3 /tmp/llm_pretrain_burn.py > /dev/null 2>&1 &
+# cd ./talas_vlm_embed
+# bash ./project_commands.sh
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python3 /tmp/llm_pretrain_burn.py > /dev/null 2>&1 &
 
 # cd ./spectral-guided-learning
 # bash ./project_commands.sh
