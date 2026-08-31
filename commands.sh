db5e17d7dbab09da8594c79b2461f6e585c2c339
phongdq25 <nothing>
2026-08-31 21:24:30 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
 #1 +60
-#talas
+#talas-test
 #v1
 
 #2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json/ +a
@@ -8,8 +8,8 @@
 # CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
 # CUDA_VISIBLE_DEVICES=6,7 python3 /tmp/llm_pretrain_burn.py &
 
-# kill -9 $(nvidia-smi -i 0,1,2,3 --query-compute-apps=pid --format=csv,noheader)
-# sleep 3
+kill -9 $(nvidia-smi -i 6,7 --query-compute-apps=pid --format=csv,noheader)
+sleep 3
 nvidia-smi
 
 export PATH=/usr/local/cuda/bin:$PATH
@@ -19,10 +19,11 @@ export TRANSFORMERS_OFFLINE=1
 export HF_DATASETS_OFFLINE=1
 
 
-cd ./talas_vlm_embed
-bash ./project_commands.sh
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
-CUDA_VISIBLE_DEVICES=0,1,2,3 python3 /tmp/llm_pretrain_burn.py &
+# cd ./talas_vlm_embed
+# # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
+# CUDA_VISIBLE_DEVICES=6,7 python3 multi_gpu.py &
+# bash ./project_commands.sh
+# # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 /tmp/llm_pretrain_burn.py &
 # CUDA_VISIBLE_DEVICES=6,7 python3 /tmp/llm_pretrain_burn.py &
 
 # cd ./spectral-guided-learning
