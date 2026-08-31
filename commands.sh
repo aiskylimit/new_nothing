3aea1fa9246d1eb5554fb3cb33d4445f571a0184
phongdq25 <nothing>
2026-08-31 14:20:19 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,36 +1,9 @@
-#1 +60
-#talas-vqa
-#v1
-
-#2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json +a
-
-# nvidia-smi
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
-# CUDA_VISIBLE_DEVICES=6,7 python3 /tmp/llm_pretrain_burn.py &
-
-kill -9 $(nvidia-smi -i 6,7 --query-compute-apps=pid --format=csv,noheader)
-# sleep 3
-nvidia-smi
-
-export PATH=/usr/local/cuda/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
-export HF_HUB_OFFLINE=1
-export TRANSFORMERS_OFFLINE=1
-export HF_DATASETS_OFFLINE=1
-
-
-cd ./talas_vlm_embed
-bash ./project_commands.sh
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python3 /tmp/llm_pretrain_burn.py &
-CUDA_VISIBLE_DEVICES=6,7 python3 /tmp/llm_pretrain_burn.py &
-
-# cd ./spectral-guided-learning
-# bash ./project_commands.sh
-
-# cd ./reward-guidance-main
-# bash ./project_command.sh
-
-# cd ./VLM_Distillation-main
-# bash ./project_commands.sh
-# CUDA_VISIBLE_DEVICES=4,5 python3 /tmp/llm_pretrain_burn.py &
-
+#d
+#datasets
+--url https://huggingface.co/datasets/TIGER-Lab/MMEB-train/resolve/main/images_zip/A-OKVQA.zip /mnt/local/aiskylimit_new_nothing/talas_vlm_embed/datasets
+--url https://huggingface.co/datasets/TIGER-Lab/MMEB-train/resolve/main/images_zip/ChartQA.zip /mnt/local/aiskylimit_new_nothing/talas_vlm_embed/datasets
+--url https://huggingface.co/datasets/TIGER-Lab/MMEB-train/resolve/main/images_zip/DocVQA.zip /mnt/local/aiskylimit_new_nothing/talas_vlm_embed/datasets
+--url https://huggingface.co/datasets/TIGER-Lab/MMEB-train/resolve/main/images_zip/InfographicsVQA.zip /mnt/local/aiskylimit_new_nothing/talas_vlm_embed/datasets
+--url https://huggingface.co/datasets/TIGER-Lab/MMEB-train/resolve/main/images_zip/Visual7W.zip /mnt/local/aiskylimit_new_nothing/talas_vlm_embed/datasets
+--url https://huggingface.co/datasets/TIGER-Lab/MMEB-train/resolve/main/images_zip/OK-VQA.zip /mnt/local/aiskylimit_new_nothing/talas_vlm_embed/datasets
+--url https://huggingface.co/datasets/TIGER-Lab/MMEB-train/resolve/main/images_zip/MSCOCO.zip /mnt/local/aiskylimit_new_nothing/talas_vlm_embed/datasets
