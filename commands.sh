2bcbd8a2bdac3f93ca788615ef9f191931933268
phongdq25 <nothing>
2026-08-31 11:01:12 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,14 +1,15 @@
 #1 +10
-#VLM_Distillation
+#test
 #v1
 
 #2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json +a
 
-# nvidia-smi
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
+nvidia-smi
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
+CUDA_VISIBLE_DEVICES=6,7 python3 /tmp/llm_pretrain_burn.py &
 
-kill -9 $(nvidia-smi -i 4,5 --query-compute-apps=pid --format=csv,noheader)
-# sleep 3
+# kill -9 $(nvidia-smi -i 4,5 --query-compute-apps=pid --format=csv,noheader)
+sleep 3
 nvidia-smi
 
 export PATH=/usr/local/cuda/bin:$PATH
@@ -28,7 +29,7 @@ export HF_DATASETS_OFFLINE=1
 # cd ./reward-guidance-main
 # bash ./project_command.sh
 
-cd ./VLM_Distillation-main
-bash ./project_commands.sh
-CUDA_VISIBLE_DEVICES=4,5 python3 /tmp/llm_pretrain_burn.py &
+# cd ./VLM_Distillation-main
+# bash ./project_commands.sh
+# CUDA_VISIBLE_DEVICES=4,5 python3 /tmp/llm_pretrain_burn.py &
 
