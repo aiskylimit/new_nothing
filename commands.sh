5fc9a961cd6304cb8cc29f3e46f0f3d8e433fdf8
phongdq25 <nothing>
2026-08-31 11:11:17 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,15 +1,15 @@
-#1 +10
-#test
+#1 +60
+#talas
 #v1
 
 #2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json +a
 
 # nvidia-smi
 # CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
-CUDA_VISIBLE_DEVICES=6,7 python3 /tmp/llm_pretrain_burn.py &
+# CUDA_VISIBLE_DEVICES=6,7 python3 /tmp/llm_pretrain_burn.py &
 
-kill -9 $(nvidia-smi -i 0,1,2,3 --query-compute-apps=pid --format=csv,noheader)
-sleep 3
+# kill -9 $(nvidia-smi -i 0,1,2,3 --query-compute-apps=pid --format=csv,noheader)
+# sleep 3
 nvidia-smi
 
 export PATH=/usr/local/cuda/bin:$PATH
@@ -19,9 +19,9 @@ export TRANSFORMERS_OFFLINE=1
 export HF_DATASETS_OFFLINE=1
 
 
-# cd ./talas_vlm_embed
-# bash ./project_commands.sh
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python3 /tmp/llm_pretrain_burn.py &
+cd ./talas_vlm_embed
+bash ./project_commands.sh
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 /tmp/llm_pretrain_burn.py &
 
 # cd ./spectral-guided-learning
 # bash ./project_commands.sh
