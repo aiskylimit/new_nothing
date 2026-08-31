d9aed7dbaddcf0e01dbdefd2b636b2bed5bbb732
phongdq25 <nothing>
2026-08-31 11:07:39 +0700

    update

--- commands.sh
+++ commands.sh
@@ -4,12 +4,12 @@
 
 #2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json +a
 
-nvidia-smi
+# nvidia-smi
 # CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
 # CUDA_VISIBLE_DEVICES=6,7 python3 /tmp/llm_pretrain_burn.py &
 
-kill -9 $(nvidia-smi -i 0,1,2,3,4,5,6,7 --query-compute-apps=pid --format=csv,noheader)
-sleep 3
+# kill -9 $(nvidia-smi -i 0,1,2,3,4,5,6,7 --query-compute-apps=pid --format=csv,noheader)
+# sleep 3
 nvidia-smi
 
 export PATH=/usr/local/cuda/bin:$PATH
