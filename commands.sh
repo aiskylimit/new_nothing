8c92ff280dd6a41421e04d76520c8197c5e3201b
phongdq25 <nothing>
2026-09-01 13:25:56 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,44 +1,38 @@
-#d
-#datasets
---url https://huggingface.co/datasets/VoCuc/vlm-teacher-embedding/resolve/main/B3_Qwen2_2B_vqa.tar.gz /mnt/local/aiskylimit_new_nothing/talas_vlm_embed/datasets
-
-
-
-#1 +10
-#test
+#1 +120
+#talas
 #v1
 
-# #2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json/ +a
+#2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json/ +a
 
-# # nvidia-smi
-# # CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
+# nvidia-smi
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
+# CUDA_VISIBLE_DEVICES=6,7 python3 /tmp/llm_pretrain_burn.py &
+
+kill -9 $(nvidia-smi -i 0,1,2,3,4,5,6,7 --query-compute-apps=pid --format=csv,noheader)
+# sleep 3
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
+nvidia-smi
+
+export PATH=/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+export HF_HUB_OFFLINE=1
+export TRANSFORMERS_OFFLINE=1
+export HF_DATASETS_OFFLINE=1
+
+cd ./talas_vlm_embed
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 multi_gpu.py &
+# # CUDA_VISIBLE_DEVICES=6,7 python3 multi_gpu.py &
+bash ./project_commands.sh
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
 # # CUDA_VISIBLE_DEVICES=6,7 python3 /tmp/llm_pretrain_burn.py &
 
-# kill -9 $(nvidia-smi -i 0,1,2,3,4,5,6,7 --query-compute-apps=pid --format=csv,noheader)
-# # sleep 3
-# # CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
-# nvidia-smi
-
-# export PATH=/usr/local/cuda/bin:$PATH
-# export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
-# export HF_HUB_OFFLINE=1
-# export TRANSFORMERS_OFFLINE=1
-# export HF_DATASETS_OFFLINE=1
-
-# cd ./talas_vlm_embed
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 multi_gpu.py &
-# # # CUDA_VISIBLE_DEVICES=6,7 python3 multi_gpu.py &
+# cd ./spectral-guided-learning
 # bash ./project_commands.sh
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
-# # # CUDA_VISIBLE_DEVICES=6,7 python3 /tmp/llm_pretrain_burn.py &
 
-# # cd ./spectral-guided-learning
-# # bash ./project_commands.sh
+# cd ./reward-guidance-main
+# bash ./project_command.sh
 
-# # cd ./reward-guidance-main
-# # bash ./project_command.sh
-
-# # cd ./VLM_Distillation-main
-# # bash ./project_commands.sh
-# # CUDA_VISIBLE_DEVICES=4,5 python3 /tmp/llm_pretrain_burn.py &
+# cd ./VLM_Distillation-main
+# bash ./project_commands.sh
+# CUDA_VISIBLE_DEVICES=4,5 python3 /tmp/llm_pretrain_burn.py &
 
