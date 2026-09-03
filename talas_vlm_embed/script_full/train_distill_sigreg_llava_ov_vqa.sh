010a708ca507a5b001b2b0f69439317a3c6c51b4
phongdq25 <nothing>
2026-09-03 10:37:20 +0700

    update

--- talas_vlm_embed/script_full/train_distill_sigreg_llava_ov_vqa.sh
+++ talas_vlm_embed/script_full/train_distill_sigreg_llava_ov_vqa.sh
@@ -192,7 +192,7 @@ python eval_mmeb.py \
     --dataset_name vlm2vec_eval/MMEB-eval \
     --subset_name "${SUBSETS[@]}" \
     --dataset_split test \
-    --per_device_eval_batch_size 32 \
+    --per_device_eval_batch_size 16 \
     --image_dir eval_images/ \
     --image_resolution "low" \
     --tgt_prefix_mod \
