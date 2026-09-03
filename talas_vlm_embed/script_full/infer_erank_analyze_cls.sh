4bf8d69a4ea51ea4dac4fae7d446a9a24ca4e082
phongdq25 <nothing>
2026-09-03 08:43:03 +0700

    update

--- talas_vlm_embed/script_full/infer_erank_analyze_cls.sh
+++ talas_vlm_embed/script_full/infer_erank_analyze_cls.sh
@@ -70,9 +70,9 @@ D_SIGREG=$(bool_to_int "$USE_SIGREG_LOSS")
 # Tên experiment
 # ============================================================
 
-EXP_NAME="talas_jepa_cls_d${D_DISTILL}_cse${D_CSE}_vis${D_VISION}_sig${D_SIGREG}_kd${KD_WEIGHT}_sw${SIGREG_WEIGHT}"
+EXP_NAME="talas_jepa_v4_d${D_DISTILL}_cse${D_CSE}_vis${D_VISION}_sig${D_SIGREG}_kd${KD_WEIGHT}_sw${SIGREG_WEIGHT}"
 
-MODEL="training/FastVLM-0.5B_${EXP_NAME}/checkpoint-epoch-0"
+MODEL="training/FastVLM-0.5B_cls_${EXP_NAME}/checkpoint-epoch-0"
 
 
 
