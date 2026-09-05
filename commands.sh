6463e392f2993c908e44cbb5fc2822dd12be4927
phongdq25 <nothing>
2026-09-05 12:22:53 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,6 +1,6 @@
 #2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json/ +a
 #talas-v8
-#v1
+#v2
 
 #2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json/ +a
 
