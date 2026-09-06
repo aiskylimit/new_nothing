d870ba6236d617033374029f5a9a2adefd355623
phongdq25 <nothing>
2026-09-06 22:56:40 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
-#2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json-v2/ +a
-#talas-v9
+#1 +10
+#test
 #v2
 
 #2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json-v2/ +a
@@ -20,6 +20,8 @@ export HF_HUB_OFFLINE=1
 export TRANSFORMERS_OFFLINE=1
 export HF_DATASETS_OFFLINE=1
 export NCCL_DEBUG=WARN
+
+rm -rf /mnt/local/uvenvs/vlm-distillation-eval
 
 # cd ./talas_vlm_embed
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
