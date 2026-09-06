9abc3b046450bc0c4a0599667f07ef20f5b1c2e6
phongdq25 <nothing>
2026-09-06 09:47:59 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,8 +1,9 @@
-#2 -0-25
+#2 -f-/mnt/local/aiskylimit_new_nothing/_run_log_/_run-2026-09-03_17-01-16-VLM-Distillation.log
 #test
 #v2
 
 #2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json-v2/ +a
+#2 -f-/mnt/local/aiskylimit_new_nothing/_run_log_/_run-2026-09-03_17-01-16-VLM-Distillation.log
 
 # nvidia-smi
 # CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
