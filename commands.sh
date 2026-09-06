586ee513c08bf2e1874072f5e2932c6722c44145
phongdq25 <nothing>
2026-09-06 07:41:07 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,8 +1,8 @@
-#2 -0-5
+#2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json-v2/ +a
 #test
 #v2
 
-#2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json/ +a
+#2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json-v2/ +a
 
 # nvidia-smi
 # CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
