3dcd622fa2d91cdcc8bd76d41d46dd5df15a77d3
DanhVinhLe <danhvinhlee@gmail.com>
2026-09-07 22:44:07 +0700

    update script

--- VLM_Distillation-main/project_commands_eval.sh
+++ VLM_Distillation-main/project_commands_eval.sh
@@ -2,7 +2,7 @@
 set -e
 source /mnt/local/uvenvs/vlm-distill-eval/bin/activate
 
-export CUDA_VISIBLE_DEVICES=2,3
+export CUDA_VISIBLE_DEVICES=2
 bash "scripts/eval/run_one_trained_checkpoint copy.sh"
 bash "scripts/eval/run_one_trained_checkpoint copy 2.sh"
-bash "scripts/eval/run_one_trained_checkpoint copy 3.sh"
+# bash "scripts/eval/run_one_trained_checkpoint copy 3.sh"
