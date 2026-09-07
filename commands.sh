45bc2182091ec1bad638a52420f47712b4d3f4f7
phongdq25 <nothing>
2026-09-07 14:08:05 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#2 -0-5
+#i vlm-distill-eval.txt
 #erank
 #v2
 
@@ -22,11 +22,11 @@ export TRANSFORMERS_OFFLINE=1
 export HF_DATASETS_OFFLINE=1
 export NCCL_DEBUG=WARN
 
-cd ./talas_vlm_embed
+# cd ./talas_vlm_embed
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
 # CUDA_VISIBLE_DEVICES=6,7 python3 multi_gpu.py &
-bash ./project_commands.sh
+# bash ./project_commands.sh
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
