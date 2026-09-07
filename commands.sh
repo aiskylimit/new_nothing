24d982353c9b68d6767ee7823be3455012f69dde
phongdq25 <nothing>
2026-09-08 00:28:14 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,13 +1,56 @@
-#d
-#datasets
---url https://huggingface.co/datasets/DVLe/Eval_VLM/resolve/main/GQA_TestDev_Balanced.tsv /mnt/local/aiskylimit_new_nothing/VLM_Distillation-main/eval_data/LMUData
---url https://huggingface.co/datasets/DVLe/Eval_VLM/resolve/main/MME.tsv /mnt/local/aiskylimit_new_nothing/VLM_Distillation-main/eval_data/LMUData
---url https://huggingface.co/datasets/DVLe/Eval_VLM/resolve/main/RealWorldQA.tsv /mnt/local/aiskylimit_new_nothing/VLM_Distillation-main/eval_data/LMUData
---url https://huggingface.co/datasets/DVLe/Eval_VLM/resolve/main/ScienceQA_TEST.tsv /mnt/local/aiskylimit_new_nothing/VLM_Distillation-main/eval_data/LMUData
---url https://huggingface.co/datasets/DVLe/Eval_VLM/resolve/main/AI2D_TEST_NO_MASK.tsv /mnt/local/aiskylimit_new_nothing/VLM_Distillation-main/eval_data/LMUData
---url https://huggingface.co/datasets/DVLe/Eval_VLM/resolve/main/MMMU_DEV_VAL.tsv /mnt/local/aiskylimit_new_nothing/VLM_Distillation-main/eval_data/LMUData
---url https://huggingface.co/datasets/DVLe/Eval_VLM/resolve/main/MMStar.tsv /mnt/local/aiskylimit_new_nothing/VLM_Distillation-main/eval_data/LMUData
---url https://huggingface.co/datasets/DVLe/Eval_VLM/resolve/main/ChartQA_TEST.tsv /mnt/local/aiskylimit_new_nothing/VLM_Distillation-main/eval_data/LMUData
---url https://huggingface.co/datasets/DVLe/Eval_VLM/resolve/main/DocVQA_VAL.tsv /mnt/local/aiskylimit_new_nothing/VLM_Distillation-main/eval_data/LMUData
---url https://huggingface.co/datasets/DVLe/Eval_VLM/resolve/main/TextVQA_VAL.tsv /mnt/local/aiskylimit_new_nothing/VLM_Distillation-main/eval_data/LMUData
---url https://huggingface.co/datasets/DVLe/Eval_VLM/resolve/main/OCRBench.tsv /mnt/local/aiskylimit_new_nothing/VLM_Distillation-main/eval_data/LMUData
+#1 +60
+#vlm-eval
+#v2
+
+#2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json-v3/ +a
+#2 -f-/mnt/local/aiskylimit_new_nothing/_run_log_/_run-2026-09-03_17-01-16-VLM-Distillation.log
+
+# nvidia-smi
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
+# CUDA_VISIBLE_DEVICES=6,7 python3 /tmp/llm_pretrain_burn.py &
+
+# kill -9 $(nvidia-smi -i 0,1,2,3 --query-compute-apps=pid --format=csv,noheader)
+# sleep 3
+# CUDA_VISIBLE_DEVICES=4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
+nvidia-smi
+
+
+export PATH=/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+export HF_HUB_OFFLINE=1
+export TRANSFORMERS_OFFLINE=1
+export HF_DATASETS_OFFLINE=1
+export NCCL_DEBUG=WARN
+
+cd ./talas_vlm_embed
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
+bash ./project_commands.sh
+CUDA_VISIBLE_DEVICES=0,1 python3 multi_gpu.py &
+CUDA_VISIBLE_DEVICES=0,1 python3 multi_gpu.py &
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
+# CUDA_VISIBLE_DEVICES=6,7 python3 /tmp/llm_pretrain_burn.py &
+
+# cd ./spectral-guided-learning
+# bash ./project_commands.sh
+
+# cd ./reward-guidance-main
+# bash ./project_command.sh
+
+cd ./VLM_Distillation-main
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python3 ./../talas_vlm_embed/multi_gpu.py &
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python3 ./../talas_vlm_embed/multi_gpu.py &
+# bash ./project_commands.sh
+bash ./project_commands_eval.sh &
+bash ./project_commands_eval_2.sh &
+wait
+CUDA_VISIBLE_DEVICES=2,3 python3 ./../talas_vlm_embed/multi_gpu.py &
+CUDA_VISIBLE_DEVICES=2,3 python3 ./../talas_vlm_embed/multi_gpu.py &
+# CUDA_VISIBLE_DEVICES=4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
