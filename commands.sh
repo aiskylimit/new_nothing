664a79c1651bbc4f02ca05061caaaa3330331d64
phongdq25 <nothing>
2026-09-08 19:36:11 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#2
+#1 +10
 #erank
 #v2
 
@@ -9,8 +9,8 @@
 # CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
 # CUDA_VISIBLE_DEVICES=6,7 python3 /tmp/llm_pretrain_burn.py &
 
-# kill -9 $(nvidia-smi -i 0,1 --query-compute-apps=pid --format=csv,noheader)
-# sleep 3
+kill -9 $(nvidia-smi -i 0,1 --query-compute-apps=pid --format=csv,noheader)
+sleep 3
 # CUDA_VISIBLE_DEVICES=4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
 nvidia-smi
 
@@ -22,10 +22,10 @@ export TRANSFORMERS_OFFLINE=1
 export HF_DATASETS_OFFLINE=1
 export NCCL_DEBUG=WARN
 
-cd ./talas_vlm_embed
+# cd ./talas_vlm_embed
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
-bash ./project_commands.sh
+# bash ./project_commands.sh
 # CUDA_VISIBLE_DEVICES=0,1 python3 multi_gpu.py &
 # CUDA_VISIBLE_DEVICES=0,1 python3 multi_gpu.py &
 # # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
