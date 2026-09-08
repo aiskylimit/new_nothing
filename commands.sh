ad53a537959a79ce06237647498a0a44204db4ae
phongdq25 <nothing>
2026-09-08 21:53:04 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#1 +120
+#2 -0-5
 #vlm-eval
 #v2
 
@@ -44,13 +44,13 @@ export NCCL_DEBUG=WARN
 # cd ./reward-guidance-main
 # bash ./project_command.sh
 
-cd ./VLM_Distillation-main
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python3 ./../talas_vlm_embed/multi_gpu.py &
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python3 ./../talas_vlm_embed/multi_gpu.py &
-# bash ./project_commands.sh
-bash ./project_commands_eval.sh &
-bash ./project_commands_eval_2.sh &
-wait
-CUDA_VISIBLE_DEVICES=2,3 python3 ./../talas_vlm_embed/multi_gpu.py &
-CUDA_VISIBLE_DEVICES=2,3 python3 ./../talas_vlm_embed/multi_gpu.py &
-# CUDA_VISIBLE_DEVICES=4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
+# cd ./VLM_Distillation-main
+# # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 ./../talas_vlm_embed/multi_gpu.py &
+# # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 ./../talas_vlm_embed/multi_gpu.py &
+# # bash ./project_commands.sh
+# bash ./project_commands_eval.sh &
+# bash ./project_commands_eval_2.sh &
+# wait
+# CUDA_VISIBLE_DEVICES=2,3 python3 ./../talas_vlm_embed/multi_gpu.py &
+# CUDA_VISIBLE_DEVICES=2,3 python3 ./../talas_vlm_embed/multi_gpu.py &
+# # CUDA_VISIBLE_DEVICES=4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
