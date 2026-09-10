e8263e2972262c2dd590b0e5ee1b516b51bd8ab3
phongdq25 <nothing>
2026-09-10 11:38:58 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#i reasoning-velocity-distill-eval.txt
+#1 +10
 #test
 #v1
 
@@ -45,8 +45,9 @@ export NCCL_DEBUG=WARN
 # cd ./reward-guidance-main
 # bash ./project_command.sh
 
-# cd ./VLM_Distillation-main
+cd ./VLM_Distillation-main
 # bash ./project_commands_collect_eval_summary.sh
+bash scripts/eval/collect_base_model_summaries.sh
 # ls outputs/eval/
 # ls outputs/eval/base_models
 # CUDA_VISIBLE_DEVICES=4,5,6,7 python3 ./../talas_vlm_embed/multi_gpu.py &
