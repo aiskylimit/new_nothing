c138e0729aa711bb4f3cb57b979b164d891432fb
phongdq25 <nothing>
2026-09-10 23:20:50 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,4 +1,7 @@
-#2 +a
+#d
+#datasets
+--url https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.8.0/en_core_web_sm-3.8.0-py3-none-any.whl /mnt/local/aiskylimit_new_nothing/talas_vlm_embed/en_core_web_sm-3.8.0-py3-none-any.whl
+
 #hierd
 #v1
 
