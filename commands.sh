e878999459caabc36bb45f66e3b32f6c70485774
phongdq25 <nothing>
2026-09-10 13:50:06 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
 #1 +10
-#test
+#vlm-eval
 #v1
 
 #2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json-v3/ +a
@@ -47,14 +47,14 @@ export NCCL_DEBUG=WARN
 
 cd ./VLM_Distillation-main
 # bash ./project_commands_collect_eval_summary.sh
-bash scripts/eval/collect_base_model_summaries.sh
+# bash scripts/eval/collect_base_model_summaries.sh
 # ls outputs/eval/
 # ls outputs/eval/base_models
 # CUDA_VISIBLE_DEVICES=4,5,6,7 python3 ./../talas_vlm_embed/multi_gpu.py &
 # CUDA_VISIBLE_DEVICES=4,5,6,7 python3 ./../talas_vlm_embed/multi_gpu.py &
 # # # bash ./project_commands.sh
-# bash ./project_commands_eval_base_model_1.sh &
-# bash ./project_commands_eval_base_model_2.sh &
+bash ./project_commands_eval_base_model_1.sh &
+bash ./project_commands_eval_base_model_2.sh &
 # wait
 # CUDA_VISIBLE_DEVICES=4,5,6,7 python3 ./../talas_vlm_embed/multi_gpu.py
 # # CUDA_VISIBLE_DEVICES=2,3 python3 ./../talas_vlm_embed/multi_gpu.py &
