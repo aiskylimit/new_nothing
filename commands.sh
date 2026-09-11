bc938fb2d5db9a5ec8864e96c6d4f0d79cf5cef2
phongdq25 <nothing>
2026-09-11 14:47:20 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
-#1 +60
-#test-talas
+#1 +120
+#talas-v10
 #v1
 
 #2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json-v3/ +a
@@ -11,7 +11,6 @@
 # CUDA_VISIBLE_DEVICES=6,7 python3 /tmp/llm_pretrain_burn.py &
 
 # kill -9 $(nvidia-smi -i 0,1,2,3 --query-compute-apps=pid --format=csv,noheader)\
-kill -9 628080 633263
 # sleep 3
 # CUDA_VISIBLE_DEVICES=4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
 nvidia-smi
@@ -25,18 +24,19 @@ export HF_DATASETS_OFFLINE=1
 export NCCL_DEBUG=WARN
 
 cd ./talas_vlm_embed
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
 bash ./project_commands.sh
 # # CUDA_VISIBLE_DEVICES=0,1 python3 multi_gpu.py &
 # # CUDA_VISIBLE_DEVICES=0,1 python3 multi_gpu.py &
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
-# # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
-# # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
-# # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
-# # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
 # # CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
 # # CUDA_VISIBLE_DEVICES=6,7 python3 /tmp/llm_pretrain_burn.py &
 
