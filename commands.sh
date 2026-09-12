9f9179fe7346bbe051d285a204b81528ff6cd3ac
phongdq25 <nothing>
2026-09-12 22:13:55 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#1 +10
+#2 -0-9
 #talas_vlm_embed
 #v2
 
@@ -29,8 +29,8 @@ export NCCL_DEBUG=WARN
 # CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 ./talas_vlm_embed/multi_gpu_v2.py
 
 
-cd ./talas_vlm_embed
-bash ./project_commands.sh
+# cd ./talas_vlm_embed
+# bash ./project_commands.sh
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
