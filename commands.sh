7209a9391213e1197d9a3db7de320d8b4fd3d1dc
phongdq25 <nothing>
2026-09-12 09:48:02 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
 #1 +10
-#test
+#talas
 #v1
 
 #2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json-v3/ +a
@@ -23,14 +23,14 @@ export TRANSFORMERS_OFFLINE=1
 export HF_DATASETS_OFFLINE=1
 export NCCL_DEBUG=WARN
 
-# source /mnt/local/uvenvs/talas-vlm-embed/bin/activate
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 ./talas_vlm_embed/multi_gpu_v2.py
+source /mnt/local/uvenvs/talas-vlm-embed/bin/activate
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 ./talas_vlm_embed/multi_gpu_v2.py
 
-# cd ./talas_vlm_embed
+cd ./talas_vlm_embed
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
-# bash ./project_commands.sh
+bash ./project_commands.sh
 # # CUDA_VISIBLE_DEVICES=0,1 python3 multi_gpu.py &
 # # CUDA_VISIBLE_DEVICES=0,1 python3 multi_gpu.py &
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
