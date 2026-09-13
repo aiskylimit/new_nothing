e68d03c737940c6aa1ccf810b3bf57597163f064
phongdq25 <nothing>
2026-09-13 21:49:24 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
-#2 -0-10
-#reasoning_velocity_distill
+#1 +10
+#test
 #v2
 
 #2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json-v3/ +a
@@ -26,7 +26,8 @@ export NCCL_DEBUG=WARN
 # CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python ./talas_vlm_embed/multi_gpu_v2.py
 
 
-# cd ./talas_vlm_embed
+cd ./talas_vlm_embed
+ls training
 # bash ./project_commands.sh
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
