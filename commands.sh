e197203958f047296a39babe21aa2c6c5fa147da
phongdq25 <nothing>
2026-09-13 22:07:19 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#1 +10
+#2
 #erank
 #v2
 
@@ -28,7 +28,7 @@ export NCCL_DEBUG=WARN
 
 cd ./talas_vlm_embed
 # ls training
-bash ./project_commands.sh
+# bash ./project_commands.sh
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
--- talas_vlm_embed/project_commands.sh
+++ talas_vlm_embed/project_commands.sh
@@ -37,24 +37,24 @@ source /mnt/local/uvenvs/talas-vlm-embed/bin/activate
 
 
 
-# CUDA_VISIBLE_DEVICES=0 bash script_full/train_distill_sigreg_cls.sh 1 1 1 1 1.0 1.0 9 0.02 &
-# CUDA_VISIBLE_DEVICES=0 bash script_full/train_distill_sigreg_cls.sh 1 1 1 1 1.0 0.5 9 0.02 &
-# CUDA_VISIBLE_DEVICES=0 bash script_full/train_distill_sigreg_cls.sh 1 1 1 1 1.0 0.3 9 0.02 &
-# CUDA_VISIBLE_DEVICES=0 bash script_full/train_distill_sigreg_cls.sh 1 1 1 1 1.0 0.3 9 0.05 &
-# CUDA_VISIBLE_DEVICES=0 bash script_full/train_distill_sigreg_cls.sh 1 1 1 0 1.0 5.0 9 0.05 &
+CUDA_VISIBLE_DEVICES=0 bash script_full/train_distill_sigreg_cls.sh 1 1 0 0 1.0 1.0 9 0.1 &
+CUDA_VISIBLE_DEVICES=0 bash script_full/train_distill_sigreg_cls.sh 1 1 1 0 1.0 0.5 9 0.1 &
+CUDA_VISIBLE_DEVICES=0 bash script_full/train_distill_sigreg_cls.sh 1 1 0 1 1.0 0.3 9 0.1 &
+CUDA_VISIBLE_DEVICES=0 bash script_full/train_distill_sigreg_cls.sh 1 1 1 0 1.0 0.5 9 0.02 &
+CUDA_VISIBLE_DEVICES=0 bash script_full/train_distill_sigreg_cls.sh 1 1 1 0 1.0 1.0 9 0.02 &
 # CUDA_VISIBLE_DEVICES=1 bash script_full/train_distill_sigreg_cls.sh 1 1 1 0 1.0 10.0 9 0.05 &
 # CUDA_VISIBLE_DEVICES=1 bash script_full/train_distill_sigreg_cls.sh 1 1 1 1 1.0 0.5 9 0.05 &
-# CUDA_VISIBLE_DEVICES=1 bash script_full/train_distill_sigreg_cls.sh 1 1 1 1 2.0 1.0 9 0.05 &
+CUDA_VISIBLE_DEVICES=1 bash script_full/train_distill_sigreg_cls.sh 1 1 1 1 2.0 0.5 9 0.02 &
 # # # wait
 
-# CUDA_VISIBLE_DEVICES=1 bash script_full/train_distill_sigreg_vqa.sh 1 1 1 1 1.0 1.0 9 0.02 &
+CUDA_VISIBLE_DEVICES=1 bash script_full/train_distill_sigreg_vqa.sh 1 1 1 1 1.0 0.5 9 0.02 &
 # # # CUDA_VISIBLE_DEVICES=1 bash script_full/train_distill_sigreg_vqa.sh 1 1 1 1 1.0 0.2 &
 # # # CUDA_VISIBLE_DEVICES=4 bash script_full/train_distill_sigreg_vqa.sh 1 1 1 1 1.0 0.01 &
 # # # CUDA_VISIBLE_DEVICES=4 bash script_full/train_distill_sigreg_vqa.sh 1 1 1 1 1.0 0.1 &
 # # # CUDA_VISIBLE_DEVICES=5 bash script_full/train_distill_sigreg_vqa.sh 1 1 1 1 1.0 0.2 &
 # # # wait
 
-# CUDA_VISIBLE_DEVICES=1 bash script_full/train_distill_sigreg_llava_ov_cls.sh 1 1 1 1 1.0 1.0 9 0.1 &
+CUDA_VISIBLE_DEVICES=1 bash script_full/train_distill_sigreg_llava_ov_cls.sh 1 1 1 1 1.0 0.5 9 0.02 &
 # # CUDA_VISIBLE_DEVICES=2 bash scripts/train_single_ov_eos_cls.sh &
 # # CUDA_VISIBLE_DEVICES=3 bash script_full/train_distill_sigreg_llava_ov_vqa.sh 1 1 1 1 1.0 0.1 6 1&
 # # CUDA_VISIBLE_DEVICES=6 bash script_full/train_distill_sigreg_llava_ov_cls.sh 1 1 1 1 1.0 0.05 &
@@ -68,12 +68,12 @@ source /mnt/local/uvenvs/talas-vlm-embed/bin/activate
 
 wait
 
-CUDA_VISIBLE_DEVICES=0 bash script_full/infer_erank_analyze_cls.sh 1 1 1 1 1.0 0.5 9 0.02
+# CUDA_VISIBLE_DEVICES=0 bash script_full/infer_erank_analyze_cls.sh 1 1 1 1 1.0 0.5 9 0.02
 
 # =========================
 # 9. Copy JSON eval outputs
 # =========================
 
-# JSON_FILTER_DESTINATION="${JSON_FILTER_DESTINATION:-./MMEB-evaloutputs-json-v3}"
+JSON_FILTER_DESTINATION="${JSON_FILTER_DESTINATION:-./MMEB-evaloutputs-json-v3}"
 
-# python json_filter.py ./MMEB-eval_outputs_v3 "${JSON_FILTER_DESTINATION}" --overwrite
+python json_filter.py ./MMEB-eval_outputs_v3 "${JSON_FILTER_DESTINATION}" --overwrite
