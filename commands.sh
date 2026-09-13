aa4468b747a7025e15ebb899024c9ed0115b1fb6
phongdq25 <nothing>
2026-09-13 17:04:48 +0700

    update

--- commands.sh
+++ commands.sh
@@ -11,7 +11,7 @@
 # CUDA_VISIBLE_DEVICES=6,7 python3 /tmp/llm_pretrain_burn.py &
 
 # kill -9 $(nvidia-smi -i 0,1,2,3,4,5,6,7 --query-compute-apps=pid --format=csv,noheader)
-kill -TERM -138107
+kill -TERM -144432
 # sleep 3
 # CUDA_VISIBLE_DEVICES=4,5,6,7 python3 /tmp/llm_pretrain_burn.py &
 nvidia-smi
@@ -34,8 +34,8 @@ export NCCL_DEBUG=WARN
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
 
 
-cd ./reasoning_velocity_distill
-bash ./project_commands.sh
+# cd ./reasoning_velocity_distill
+# bash ./project_commands.sh
 
 # cd ./cypher-extract
 # bash ./project_command.sh
