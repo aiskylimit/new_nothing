5c72c6cf7c6b22acf6bf2d9ba873278152e10b58
phongdq25 <nothing>
2026-09-14 22:12:56 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,16 +1,6 @@
 #1 +10
-#test
+#multi-mode-eval
 #v2
-
-for pid in /proc/[0-9]*; do
-    p=${pid##*/}
-    devs=$(ls -l "$pid"/fd/* 2>/dev/null | grep '/dev/nvidia' | sed 's/.*-> //' | sort -u | tr '\n' ' ')
-    if [ -n "$devs" ]; then
-        echo "=================================================="
-        ps -o pid,ppid,user,stat,etime,cmd -p "$p"
-        echo "GPU devices: $devs"
-    fi
-done
 
 #2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json-v1/ +a
 #2 -f-/mnt/local/aiskylimit_new_nothing/_run_log_/_run-2026-09-03_17-01-16-VLM-Distillation.log
@@ -40,8 +30,8 @@ export NCCL_DEBUG=WARN
 # bash ./project_commands.sh
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
 
-# cd ./multi-mode-distill
-# bash ./project_commands.sh
+cd ./multi-mode-distill
+bash ./project_commands.sh
 
 # cd ./cypher-extract
 # bash ./project_command.sh
