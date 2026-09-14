505f79b52240724d2d2f9a196df60ccec8a872e9
phongdq25 <nothing>
2026-09-14 09:54:01 +0700

    update

--- talas_vlm_embed/project_commands.sh
+++ talas_vlm_embed/project_commands.sh
@@ -68,7 +68,7 @@ source /mnt/local/uvenvs/talas-vlm-embed/bin/activate
 
 wait
 
-CUDA_VISIBLE_DEVICES=0 bash script_full/infer_erank_analyze_cls.sh 1 1 0 0 1.0 1.0 9 0.1
+CUDA_VISIBLE_DEVICES=0 bash script_full/infer_erank_analyze_cls.sh 1 1 0 1 1.0 0.3 9 0.1
 
 # =========================
 # 9. Copy JSON eval outputs
