bbc93f5527928f677aed27d1a881c5074ecbb123
phongdq25 <nothing>
2026-09-17 10:24:51 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#i reasoning-velocity-distill.txt
+#1 +10
 #talas
 #v1
 
@@ -19,7 +19,7 @@ export TRANSFORMERS_OFFLINE=1
 export HF_DATASETS_OFFLINE=1
 export NCCL_DEBUG=WARN
 
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python ./talas_vlm_embed/multi_gpu_v2.py
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python ./talas_vlm_embed/multi_gpu_v2.py
 
 
 # cd ./talas_vlm_embed
