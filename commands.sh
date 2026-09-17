b59f8e0876386e9c7e274d9008779877427ba6e0
phongdq25 <nothing>
2026-09-17 21:42:56 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#2 -0-5
+#1 +10
 #test
 #v2
 
@@ -8,6 +8,7 @@
 
 # nvidia-smi
 # kill -9 $(nvidia-smi -i 0,1,2,3,4,5,6,7 --query-compute-apps=pid --format=csv,noheader)
+kill -TERM -141348
 # sleep 3
 nvidia-smi
 
