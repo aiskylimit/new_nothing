0aaa5fbbfeaa2c93331f078ffaa281048a12559b
phongdq25 <nothing>
2026-09-17 20:27:51 +0700

    update

--- talas_vlm_embed/project_commands.sh
+++ talas_vlm_embed/project_commands.sh
@@ -78,7 +78,8 @@ source /mnt/local/uvenvs/talas-vlm-embed/bin/activate
 
 wait
 
-CUDA_VISIBLE_DEVICES=3 bash script_full/infer_erank_analyze_cls.sh 1 1 1 1 1.0 2.0 9 0.2
+CUDA_VISIBLE_DEVICES=3 bash script_full/infer_erank_analyze_cls.sh 1 1 1 1 1.0 1.0 9 0.02
+CUDA_VISIBLE_DEVICES=3 bash script_full/infer_erank_analyze_cls.sh 1 1 1 1 5.0 1.0 9 0.05
 CUDA_VISIBLE_DEVICES=3 bash script_full/infer_erank_analyze_cls_ov.sh 1 1 1 1 1.0 1.0 9 0.02
 # CUDA_VISIBLE_DEVICES=0 bash ./script_full/infer_erank_analyze_cls_2.sh
 
