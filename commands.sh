e8787ab62dfb2cdf5310be48b269834e516d6e36
phongdq25 <nothing>
2026-09-17 10:32:11 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#1 +10
+#1 +5
 #talas
 #v1
 
@@ -8,6 +8,7 @@
 
 # nvidia-smi
 # kill -9 $(nvidia-smi -i 0,1,2,3,4,5,6,7 --query-compute-apps=pid --format=csv,noheader)
+kill -9 498 499 500 501 502 503 504 505
 # sleep 3
 nvidia-smi
 
@@ -22,8 +23,8 @@ export NCCL_DEBUG=WARN
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python ./talas_vlm_embed/multi_gpu_v2.py
 
 
-# cd ./talas_vlm_embed
-# bash ./project_commands.sh
+cd ./talas_vlm_embed
+bash ./project_commands.sh
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
 
 # cd ./multi-mode-distill
