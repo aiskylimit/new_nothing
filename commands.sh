0b5287f9dbd6608dbf5334c9f45b2385fc344b6a
phongdq25 <nothing>
2026-09-18 16:57:13 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#2 +a
+#1 +120
 #multi-mode-distill
 #v1
 
@@ -26,10 +26,10 @@ export NCCL_DEBUG=WARN
 # bash ./project_commands.sh
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
 
-# cd ./multi-mode-distill
+cd ./multi-mode-distill
 # bash ./project_commands_opsd_ablation.sh
 # bash ./project_commands_ablation.sh
-# bash ./project_commands.sh
+bash ./project_commands.sh
 
 # cd ./cypher-extract
 # bash ./project_command.sh
