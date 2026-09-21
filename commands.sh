762960029e335bb7d41fccad715edb3a6bb45081
phongdq25 <nothing>
2026-09-22 01:02:14 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
-#1 +120
-#talas
+#1 +10
+#test
 #v2
 
 #2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json-v5/ +a
@@ -8,7 +8,6 @@
 
 # nvidia-smi
 # kill -9 $(nvidia-smi -i 0,1,2,3,4,5,6,7 --query-compute-apps=pid --format=csv,noheader)
-kill -9 16356 17078 16357 17079 16358 17080 16359 17081 16360 17082 16361 17083 16362 17084 16363 17085
 # sleep 2
 nvidia-smi
 
@@ -23,8 +22,8 @@ export NCCL_DEBUG=WARN
 # CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python ./talas_vlm_embed/multi_gpu_v2.py
 
 
-cd ./talas_vlm_embed
-bash ./project_commands.sh
+# cd ./talas_vlm_embed
+# bash ./project_commands.sh
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
 
 # cd ./multi-mode-distill
