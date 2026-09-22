77a6887648499f7541de8aa74b8148da15411505
phongdq25 <nothing>
2026-09-22 21:58:34 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,7 +1,6 @@
-#1 +10
 #d
 #datasets
-# --url https://huggingface.co/datasets/VoCuc/vlm-teacher-embedding/resolve/main/B3_Qwen2_7B_cls.tar.gz /mnt/local/@PROJECT@/talas_vlm_embed/datasets
+--url https://huggingface.co/datasets/VoCuc/vlm-teacher-embedding/resolve/main/B3_Qwen2_7B_cls.tar.gz /mnt/local/@PROJECT@/talas_vlm_embed/datasets
 
 #talas-eval
 #v2
@@ -27,7 +26,7 @@ export NCCL_DEBUG=WARN
 
 
 cd ./talas_vlm_embed
-rm -rf datasets/B3_Qwen2_7B_cls.tar.gz
+# rm -rf datasets/B3_Qwen2_7B_cls.tar.gz
 # tree -L 2 training
 # bash ./project_commands.sh
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
