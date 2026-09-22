b67f464de9d9bce88230e712445be71ba81f41a5
phongdq25 <nothing>
2026-09-22 09:41:10 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,4 +1,7 @@
-#1 +120
+#d
+#datasets
+--url https://huggingface.co/datasets/TIGER-Lab/MMEB-eval/resolve/main/MSCOCO/test-00000-of-00001.parquet /mnt/local/@PROJECT@/talas_vlm_embed/vlm2vec_eval/MMEB-eval/MSCOCO
+
 #eval-grounding
 #v1
 
