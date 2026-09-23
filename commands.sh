80642a7370430479430475c9d1fa933fbec894a0
phongdq25 <nothing>
2026-09-23 21:53:15 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
-#2 -0-10
-#talas
+#1 +10
+#rivad
 #v1
 
 #2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json-v5/ +a
@@ -25,13 +25,10 @@ export NCCL_DEBUG=WARN
 # bash ./project_commands.sh
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python3 multi_gpu.py &
 
-# cd ./multi-mode-distill
+cd ./multi-mode-distill
 # bash ./project_commands_opsd_ablation.sh
 # bash ./project_commands_ablation.sh
-# bash ./project_commands.sh
+bash ./project_commands.sh
 
 # cd ./cypher-extract
 # bash ./project_command.sh
-
-# cd ./reasoning_velocity_distill
-# bash ./project_commands.sh
--- multi-mode-distill/project_commands.sh
+++ multi-mode-distill/project_commands.sh
@@ -109,8 +109,8 @@ printf '\n[full 1/2] Train Qwen: CE + KD + cka (mag=%s, gram=%s)\n' "$MAG_WEIGHT
 : > "$CHECKPOINT_FILE"
 CUDA_DEVICES=0,1,2,3 DATA_DIR="$QWEN_DATA_DIR" \
     SAVE_PATH="$QWEN_RESULTS_ROOT/full_mag${MAG_WEIGHT}_gram${GRAM_WEIGHT}" \
-    KD_RATIO="${CE_KD_RATIO:-0.5}" GEOMETRY=0 CKA=1 \
-    MAG_WEIGHT="$MAG_WEIGHT" GRAM_WEIGHT=5.0 \
+    KD_RATIO="${CE_KD_RATIO:-0.5}" GEOMETRY=1 CKA=0 \
+    MAG_WEIGHT="$MAG_WEIGHT" GRAM_WEIGHT=100.0 \
     FINAL_CHECKPOINT_FILE="$CHECKPOINT_FILE" \
     bash scripts/qwen/train_v2_qwen2.5_14b_to_1.5b.sh "$@"
 
